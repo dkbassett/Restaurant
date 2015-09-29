913f3bd35c7c72731f4855fc0b3186385c223d85
David-University <david.hallett2@griffithuni.edu.au>
2015-09-29 14:35:16 +1000

    Update CreditCard.java
    Fixed incorrect variable naming (variable wasnt capitalised at one point)

--- OrderSystem/LogicTest/CreditCard.java
+++ OrderSystem/LogicTest/CreditCard.java
@@ -3,7 +3,7 @@ public class CreditCard {
 	private String type;	// credit provider
 	private int number;
 	
-	public CreditCard(String newCardHolder,String newtype, int newNumber){
+	public CreditCard(String newCardHolder,String newType, int newNumber){
 		cardHolder=newCardHolder;
 		type=newType; 	
 		number=newNumber;
