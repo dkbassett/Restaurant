3e7b393ccef2931aa666c31be23a51bc8db94eaf
David-University <david.hallett2@griffithuni.edu.au>
2015-09-29 10:54:42 +1000

    Create StaffMember.java

---
+++ OrderSystem/LogicTest/StaffMember.java
@@ -0,0 +1,12 @@
+public class StaffMember {
+	
+	private int staffID;
+	private String name;
+	private String password;
+	
+	public StaffMember(int newStaffID, String newName, String newPassword) {
+		staffID=newStaffID;
+		name=newName;
+		password=newPassword;
+	}		
+}
