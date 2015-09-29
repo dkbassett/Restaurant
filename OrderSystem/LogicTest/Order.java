1fe22328e4b5439e0a732a26085957670ca1df0e
David-University <david.hallett2@griffithuni.edu.au>
2015-09-29 14:29:29 +1000

    Update Order.java
    Implemented delivery type properly.

--- OrderSystem/LogicTest/Order.java
+++ OrderSystem/LogicTest/Order.java
@@ -6,14 +6,14 @@ public class Order {
 	private int customerID;
 	private ArrayList<OrderItem> itemList = new ArrayList<OrderItem>(1);
 	private boolean confirmationStatus;
-//	private DeliveryType delivery; 		Need Clarification on Implementation
+	private String delivery; 	// delivery type	
 	private float grandTotal;
 	private int cnt=0; // counter for current itemlist element
 	
-	public Order(int newId,int newCustomerID /* , DeliveryType newDelivery*/){ // Delivery Type hidden until clarified
+	public Order(int newId,int newCustomerID , String newDelivery){ 
 		id=newId;
 		customerID=newCustomerID;
-	//	delivery=newDelivery; hidden till clarified
+		delivery=newDelivery; 
 	}
 	
 	public void addItemToOrder(MenuItem newMenuItem, int newQuantity){
