d6870629acdd08f95e1f2a227615b1fca3540705
David-University <david.hallett2@griffithuni.edu.au>
2015-09-29 10:55:25 +1000

    Create Customer.java

---
+++ OrderSystem/LogicTest/Customer.java
@@ -0,0 +1,25 @@
+public class Customer{
+	
+	private int id;
+	private String name;
+	private String address;
+	private int phoneNumber;
+	private CreditCard creditCard;
+	
+	
+	public Customer(int newId, String newName, String newAddress, int newPhoneNumber, CreditCard newCreditCard){
+		id=newId;
+		name=newName;
+		address=newAddress;
+		phoneNumber=newPhoneNumber;
+		creditCard=newCreditCard;
+	}
+	
+	public void updateAddress(String updatedAddress){
+			address=updatedAddress;
+	}
+	
+	public void updateCreditCard(CreditCard updatedCreditCard){
+		creditCard=updatedCreditCard;
+	}
+	}
