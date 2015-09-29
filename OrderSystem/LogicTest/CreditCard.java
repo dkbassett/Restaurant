aa0171536fbdab969e82c8bb1c4e74cbb0e65c7e
David-University <david.hallett2@griffithuni.edu.au>
2015-09-29 10:55:51 +1000

    Create CreditCard.java

---
+++ OrderSystem/LogicTest/CreditCard.java
@@ -0,0 +1,12 @@
+public class CreditCard {
+	private String cardHolder;
+	private CreditCardProvider type;
+	private int number;
+	
+	public CreditCard(String newCardHolder, newCreditCardProvider newType, int newNumber){
+		cardHolder=newCardHolder;
+		type=newType;
+		number=newNumber;
+	}
+	
+}
