d47873f7c98fcdb059fa9ec9c73dfee16062ec72
David <David@David-PC>
2015-10-11 09:43:54 +1000

    Refined UI and added functional buttons. Cleaned up spacing, renamed
    variables

--- OrderSystem/src/gui/AddItemView.java
+++ OrderSystem/src/gui/AddItemView.java
@@ -15,46 +15,50 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 
-public class AddItemView extends JFrame{
+public class AddItemView extends JFrame implements ActionListener{
     
-	JLabel ItemID,Itemname,Itemprice; //Declaring JLabels
-	JTextField txtId,txtname,txtprice; //Declaring JTextfields
-	JButton btnCreate,btnExit; //Declaring JButtons
+	JLabel Title,ItemID,Itemname,Itemprice; //Declaring JLabels
+	JTextField txtId,txtName,txtPrice; //Declaring JTextfields
+	JButton btnConfirm,btnCancel;
   
   
    
    public AddItemView(){//Creating constructor
        setLayout(null);
+       
+       Title = new JLabel("Add Menu Item");
+       add(Title).setBounds(20,20,120,20);
         
        ItemID = new JLabel("Item ID");
-        add(ItemID).setBounds(20,20,120,20);
+        add(ItemID).setBounds(20,65,120,20);
        
         Itemname = new JLabel("Staff Name");
-        add(Itemname).setBounds(20,45,120,20);
+        add(Itemname).setBounds(20,90,120,20);
         
         Itemprice = new JLabel("Itemprice:");
-        add(Itemprice).setBounds(20,70,120,20);
+        add(Itemprice).setBounds(20,115,120,20);
         
        txtId = new JTextField();
-       add(txtId).setBounds(160,20,100,20);
+       add(txtId).setBounds(130,65,120,20);
         
        
-       txtname = new JTextField();
-       add(txtname).setBounds(160,45,100,20);
+       txtName = new JTextField();
+       add(txtName).setBounds(130,90,120,20);
        
-       txtprice = new JTextField();
-       add(txtprice).setBounds(160,65,100,20);
+       txtPrice = new JTextField();
+       add(txtPrice).setBounds(130,115,120,20);
        
        
-       btnCreate = new JButton("Save");
-       add(btnCreate).setBounds(20,170,100,20);
+	   btnConfirm = new JButton("Confirm");
+	   add(btnConfirm).setBounds(20,180,80,20);
+	   btnConfirm.addActionListener(this);
        
-       btnExit = new JButton("Exit");
-       add(btnExit).setBounds(260,170,100,20);
-       //btnExit.addActionListener(this);
+       btnCancel = new JButton("Cancel");
+       add(btnCancel).setBounds(120,180,80,20);
+       btnCancel.addActionListener(this);	
        
        setTitle(" Add Item");
-       setSize(400,500);
+       setSize(300,250);
        setVisible(true);
        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
        
@@ -62,14 +66,31 @@ public class AddItemView extends JFrame{
       
     }
 
-public void transferData(String a, String b, String c, String d, String ee,
-		String f) {
+   @Override
+   public void actionPerformed(ActionEvent e) {	
+		if(e.getSource().equals(btnCancel)){
+			//System.exit(0);
+			dispose();
+		}
+		else if(e.getSource().equals(btnConfirm)){	 
+			
+			String id = txtId.getText();
+			String name = txtName.getText();
+			String price = txtPrice.getText();
+			
+			System.out.println("Ui Level: " + id);
+			System.out.println("Ui Level: " + name);
+			System.out.println("Ui Level: " + price);
+			
+			dispose();
+	
+		}
 	
 	
-	
+   }
 }
 
 
-    }
+    
     
 
