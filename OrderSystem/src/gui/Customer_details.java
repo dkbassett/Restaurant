ef72dc14d4471f9aab5b52f78f34c1912dcebd94
David <David@David-PC>
2015-10-10 12:39:57 +1000

    Implemented New Buttons and Text Fields, Fixed exit functionality
    so that instead of quiting entire application it closes the current
    frame

--- OrderSystem/src/gui/Customer_details.java
+++ OrderSystem/src/gui/Customer_details.java
@@ -16,9 +16,9 @@ import javax.swing.JTextField;
 
 public class Customer_details extends JFrame implements ActionListener{
     
-	JLabel First_Name,Last_Name,Address, Phone, Credit_Card, CVV; //Declearing JLabels
-	JTextField txtFirst_Name,  txtLast_Name,txtAddress,txtPhone, txtCreditcard, txtCVV; //Declearing JTextfields
-	JButton btnSave,btnCreate,btnExit,btnDel; //Declearing JButtons
+	JLabel Title,Name/*,Last_Name*/,Address, Phone, Credit_Card, CVV; //Declearing JLabels
+	JTextField  txt_Name /*,  txtLast_Name*/,txtAddress,txtPhone, txtCreditcard, txtCVV; //Declearing JTextfields
+	JButton btnConfirm,btnCancel,btnExit,btnDel; //Declearing JButtons
   
    //Connection con; // Declearing Connection
    //PreparedStatement ps;// Declearing PreparedStatement
@@ -28,70 +28,74 @@ public class Customer_details extends JFrame implements ActionListener{
    public Customer_details(){//Creating constructor
        setLayout(null);
         
-        First_Name = new JLabel("First Name:");
-        add(First_Name).setBounds(20,20,120,20);
        
-        Last_Name = new JLabel("Last Name:");
-        add(Last_Name).setBounds(20,45,120,20);
+       Title = new JLabel("New Customer");
+       add(Title).setBounds(20,20,120,20);
+       
+        Name = new JLabel("Name:");  // Changed from first name, to whole name
+        add(Name).setBounds(20,65,120,20);
+       
+   //     Last_Name = new JLabel("Last Name:");
+   //     add(Last_Name).setBounds(20,45,120,20);
         
         Address = new JLabel("Address:");
-        add(Address).setBounds(20,70,120,20);
+        add(Address).setBounds(20,90,120,20);
         
         Phone = new JLabel("Phone:");
-        add(Phone).setBounds(20,95,120,20);
+        add(Phone).setBounds(20,140,120,20);
         
-        Credit_Card = new JLabel("Credit card:");
-        add(Credit_Card).setBounds(20,120,120,20);
+       // Credit_Card = new JLabel("Credit card:");
+       // add(Credit_Card).setBounds(20,120,120,20);
         
 
-        CVV = new JLabel("CVV:");
-        add(CVV).setBounds(20,145,120,20);
+      //  CVV = new JLabel("CVV:");
+     //   add(CVV).setBounds(20,145,120,20);
         
         
         
        
         
         
-       txtFirst_Name = new JTextField();
-       add(txtFirst_Name).setBounds(160,20,100,20);
+       txt_Name = new JTextField();
+       add(txt_Name).setBounds(90,65,200,20);
         
-       txtLast_Name = new JTextField();
-       add(txtLast_Name).setBounds(160,45,100,20);
+   //    txtLast_Name = new JTextField();
+   //    add(txtLast_Name).setBounds(160,45,100,20);
         
        txtAddress = new JTextField();
-       add(txtAddress).setBounds(160,70,100,20);
+       add(txtAddress).setBounds(90,90,200,40);
         
        txtPhone = new JTextField();
-       add(txtPhone).setBounds(160,95,100,20);
+       add(txtPhone).setBounds(90,140,200,20);
        
-       txtCreditcard = new JTextField();
-       add(txtCreditcard).setBounds(160,120,100,20);
+    //   txtCreditcard = new JTextField();
+    //   add(txtCreditcard).setBounds(160,120,100,20);
        
-       txtCVV = new JTextField();
-       add(txtCVV).setBounds(160,145,100,20);
+     //  txtCVV = new JTextField();
+     //  add(txtCVV).setBounds(160,145,100,20);
        
        
        
-       btnSave = new JButton("Save");
-       add(btnSave).setBounds(20,170,100,20);
-       //btnSave.addActionListener(this);
+       btnConfirm = new JButton("Confirm");
+       add(btnConfirm).setBounds(20,400,80,20);
+       btnConfirm.addActionListener(this);
        
-       btnCreate = new JButton("Create");
-       add(btnCreate).setBounds(140,170,100,20);
-       //btnCreate.addActionListener(this);
+       btnCancel = new JButton("Cancel");
+       add(btnCancel).setBounds(120,400,80,20);
+       btnCancel.addActionListener(this);
        
-       btnExit = new JButton("Exit");
+   /*    btnExit = new JButton("Exit");
        add(btnExit).setBounds(260,170,100,20);
        btnExit.addActionListener(this);
        
        btnDel = new JButton("Delete");
        add(btnDel).setBounds(140,170,100,20);
-       //btnDel.addActionListener(this);
+     */ //btnDel.addActionListener(this);
        
       
        
        
-       setTitle("Customer Details");
+       setTitle("New Customer");
        setSize(400,500);
        setVisible(true);
        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -109,8 +113,20 @@ public void transferData(String a, String b, String c, String d, String ee,
 
 @Override
 public void actionPerformed(ActionEvent e) {
-	if(e.getSource().equals(btnExit)){
-		System.exit(0);
+	if(e.getSource().equals(btnCancel)){
+		//System.exit(0);
+		dispose();
+	}
+	else if(e.getSource().equals(btnConfirm)){
+		String cust = txt_Name.getText();
+		System.out.println("Ui Level: " + cust);
+		String address = txtAddress.getText();
+		System.out.println("Ui Level: " + address);
+		String pNumber = txtPhone.getText();
+		System.out.println("Ui Level: " + pNumber);
+		dispose();
+		//Customer_details.Close();
+		//new Customer(Cust,)
 	}
 	
 }
