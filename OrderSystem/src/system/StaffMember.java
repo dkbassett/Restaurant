6a3118557f4df5bf57124641b83f983691349476
David <David@David-PC>
2015-10-11 12:40:18 +1000

    Added parsing for String input to store in StaffID variable, also debug
    info

--- OrderSystem/src/system/StaffMember.java
+++ OrderSystem/src/system/StaffMember.java
@@ -6,10 +6,16 @@ public class StaffMember {
 	private String name;
 	private String password;
 	
-	public StaffMember(int newStaffID, String newName, String newPassword) {
-		staffID=newStaffID;
+	public StaffMember(String newStaffID, String newName, String newPassword) {
+		System.out.println("StaffMember-Logic");
+		staffID=Integer.parseInt(newStaffID);
 		name=newName;
 		password=newPassword;
+		
+		System.out.println("Logic Level: " + staffID);
+		System.out.println("Logic Level: " + name);
+		System.out.println("Logic Level: " + password);
+		
 	}		
 	public int getStaffId(){
 		return (int) (staffID);
