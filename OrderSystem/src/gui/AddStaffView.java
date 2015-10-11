6829dd252718217e6676e667eb075ad0df70dacb
David <David@David-PC>
2015-10-11 10:44:06 +1000

    Added missing password field to the UI and adjusted UI to fit.

--- OrderSystem/src/gui/AddStaffView.java
+++ OrderSystem/src/gui/AddStaffView.java
@@ -15,8 +15,8 @@ import system.*;
 
 public class AddStaffView extends JFrame implements ActionListener{
     
-	JLabel Title,StaffID,Staffname,Startedfrom, Phone; //Declaring JLabels
-	JTextField txtStaffId,txtStaffname,txtStartedfrom, txtPhone; //Declaring JTextfields
+	JLabel Title,StaffID,Staffname,Startedfrom, Phone,Password; //Declaring JLabels
+	JTextField txtStaffId,txtStaffname,txtStartedfrom, txtPhone, txtPassword; //Declaring JTextfields
 	JButton btnConfirm,btnCancel;
      
    public AddStaffView(){//Creating constructor
@@ -37,6 +37,9 @@ public class AddStaffView extends JFrame implements ActionListener{
         Phone = new JLabel("Phone:");
         add(Phone).setBounds(20,140,120,20);
         
+        Password = new JLabel("Password:");
+        add(Password).setBounds(20,165,120,20);
+        
         txtStaffId = new JTextField();
        add(txtStaffId).setBounds(120,65,100,20);
         
@@ -49,16 +52,19 @@ public class AddStaffView extends JFrame implements ActionListener{
        txtPhone= new JTextField();
        add(txtPhone).setBounds(120,140,100,20);
        
+       txtPassword= new JTextField();
+       add(txtPassword).setBounds(120,165,100,20);
+       
 	   btnConfirm = new JButton("Confirm");
-	   add(btnConfirm).setBounds(20,175,80,20);
+	   add(btnConfirm).setBounds(20,200,80,20);
 	   btnConfirm.addActionListener(this);
        
 	   btnCancel = new JButton("Cancel");
-	   add(btnCancel).setBounds(120,175,80,20);
+	   add(btnCancel).setBounds(120,200,80,20);
 	   btnCancel.addActionListener(this);	 
   
        setTitle(" Add Staff");
-       setSize(300,250);
+       setSize(300,275);
        setVisible(true);
        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
@@ -76,12 +82,13 @@ public class AddStaffView extends JFrame implements ActionListener{
    			String name = txtStaffname.getText();
    			String startDate = txtStartedfrom.getText();
    			String phone = txtPhone.getText();
+   			String password = txtPassword.getText();
 
-   			
    			System.out.println("Ui Level: " + id);
    			System.out.println("Ui Level: " + name);
    			System.out.println("Ui Level: " + startDate);
-   			System.out.println("Ui Level: " + phone);  					
+   			System.out.println("Ui Level: " + phone);  		
+   			System.out.println("Ui Level: " + password);  	
 
    			dispose();
    			
